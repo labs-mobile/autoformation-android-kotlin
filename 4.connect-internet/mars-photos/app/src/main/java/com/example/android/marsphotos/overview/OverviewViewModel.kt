1f14b05795846cce8560e3751dbe62b72192e01a
ESSARRAJ Fouad <fouad.systems@gmail.com>
2023-05-10 21:41:04 +0100

    Retrofit

--- 4.connect-internet/mars-photos/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ 4.connect-internet/mars-photos/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -19,7 +19,9 @@ package com.example.android.marsphotos.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsphotos.network.MarsApi
+import kotlinx.coroutines.launch
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
@@ -42,6 +44,17 @@ class OverviewViewModel : ViewModel() {
      * [MarsPhoto] [List] [LiveData].
      */
     private fun getMarsPhotos() {
-        _status.value = "Set the Mars API status response here!"
+        viewModelScope.launch {
+            try {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                _status.value = "Success: ${listResult.size} Mars photos retrieved"
+            } catch (e: Exception) {
+                _status.value = "Failure: ${e.message}"
+            }
+        }
+    }
+
+    public fun setValue(msg:String){
+        _status.value = msg
     }
 }
